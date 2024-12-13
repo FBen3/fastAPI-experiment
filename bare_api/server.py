ce6743c8eedefd258d30aae6beb0a3f0f456de67
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-12-13 22:10:17 +0000

    Add POST endpoint

--- bare_api/server.py
+++ bare_api/server.py
@@ -5,7 +5,6 @@ import json
 class SimpleAPIHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
@@ -18,7 +17,29 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_error(404, "Endpoint not found")
 
     def do_POST(self):
-        pass
+        if self.path == "/process":
+            content_length = int(self.headers["Content-Length"])
+            post_data = self.rfile.read(content_length)
+
+            try:
+                data = json.loads(post_data)
+
+                if "value" not in data:
+                    raise ValueError("Missing 'value' in request body")
+        
+                response = {"received_value": data["value"]}
+
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+
+                self.wfile.write(json.dumps(response).encode())
+            except (json.JSONDecodeError, ValueError) as e:
+                self.send_error(400, str(e))
+
+        else:
+            self.send_error(404, "Endpoint not found")
+
 
 
 def run():
