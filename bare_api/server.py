f1e03f5d603d03421c8efcfd412e5049b0e08e73
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-12-14 22:07:30 +0000

    Add '/save' POST endpoint

--- bare_api/server.py
+++ bare_api/server.py
@@ -1,8 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
 import json
 
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
+
+    storage = {"stored_data": []}
+
 
     def do_GET(self):
         if self.path == "/":
@@ -19,7 +23,16 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "application/json")
             self.end_headers()
 
-            response = {"message": "To store data on the server, hit the /save POST endpoint"}
+            if self.storage["stored_data"]:
+                response = {
+                    "message": "Stored data retreived successfully",
+                    "data": self.storage["stored_data"]
+                }
+            else:
+                response = {
+                    "message": "To store data on the server, hit the /save POST endpoint",
+                    "data": []
+                }
 
             self.wfile.write(json.dumps(response).encode())
 
@@ -45,6 +58,30 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
 
                 self.wfile.write(json.dumps(response).encode())
             except (json.JSONDecodeError, ValueError) as e:
+                self.send_error(400, str(e))
+
+        elif self.path == "/save":
+            content_length = int(self.headers["Content-Length"])
+            post_data = self.rfile.read(content_length)
+
+            try:
+                data = json.loads(post_data)
+
+                if "value" not in data:
+                    raise ValueError("Missing 'value' in request body")
+
+                current_time = datetime.now()
+                formatted_time = current_time.strftime('%H:%M')
+                self.storage["stored_data"].append({formatted_time: data["value"]})
+
+                response = {"message": f"{data["value"]} saved successfully!"}
+
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+
+                self.wfile.write(json.dumps(response).encode())
+            except (ValueError) as e:
                 self.send_error(400, str(e))
 
         else:
